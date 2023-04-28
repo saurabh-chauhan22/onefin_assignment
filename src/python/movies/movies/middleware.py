bd3c3fc890865b9ddc9312097fcf1c4493db86cc
saurabharies22 <65561885+saurabharies22@users.noreply.github.com>
2023-04-28 09:08:51 +0530

    Fixed the middleware request count

--- src/python/movies/movies/middleware.py
+++ src/python/movies/movies/middleware.py
@@ -1,18 +1,24 @@
-import threading
 
 class RequestCounterMiddleware:
-    _lock = threading.Lock()
-    counter = 0
-    
+    '''
+    Request count middleware to count the number of request made to all the api
+    to the server
+    '''
     def __init__(self, get_response):
         self.get_response = get_response
-    
+        self.counter = 0
+
     def __call__(self, request):
         self.process_request(request)
         response = self.get_response(request)
         return response
     
     def process_request(self, request):
-        print("CALL TO MIDDLEWARE")
-        with self._lock:
-            self.counter += 1
+        # print("CALL TO MIDDLEWARE")
+        self.counter += 1
+        # print(f"Count:{self.counter}")
+        request.request_counter = self
+
+
+    def get_request_count(self):
+        return self.counter

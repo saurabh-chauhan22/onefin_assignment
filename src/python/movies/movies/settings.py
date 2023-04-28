1162c9a8e06a6dfe04764321b586d15c0105983b
saurabharies22 <65561885+saurabharies22@users.noreply.github.com>
2023-04-28 09:13:59 +0530

    Added pagination settings

--- src/python/movies/movies/settings.py
+++ src/python/movies/movies/settings.py
@@ -57,6 +57,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'movies.urls'
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
